4702f61553d1d05581c64d8800a13f37f97e9d7f
unknown <rochagasdiniz@gmail.com~>
2024-03-16 19:01:43 +0000

    Fix build

---
+++ BlazorSchool/Pages/BaseSavePage.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorSchool;
+
+public class BaseSavePage<T>:ComponentBase
+{
+        [Parameter]
+        public int? Id { get; set; }
+
+        [Inject] 
+        private  HttpClient _client { get; set; }
+        
+        [Inject] 
+        private  NavigationManager _Manager { get; set; }
+
+        private string _url{ get; set; }
+        public BaseSavePage(string url)
+        {
+            _url=url;
+        }
+
+        protected T dto{ get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            if (Id.HasValue)
+            {
+                dto = (await _client.GetFromJsonAsync<T>($"{_url}/{Id.Value}"))!;
+            }
+        }
+        protected async Task<bool> Save()
+        {
+            HttpResponseMessage message;
+            if (Id.HasValue)
+            {
+               message= await _client.PutAsJsonAsync($"{_url}/{Id.Value}", dto);
+            }
+            else
+            {
+               message=await _client.PostAsJsonAsync($"/{_url}", dto);
+            }   
+            return message.IsSuccessStatusCode;        
+        }
+}
--- BlazorSchool/Pages/Courses/SaveCourse.razor.cs
+++ BlazorSchool/Pages/Courses/SaveCourse.razor.cs
@@ -1,42 +1,28 @@
-﻿using System.Net.Http.Json;
-using BlazorSchoolShared.Dto;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 
 namespace BlazorSchool.Pages.Courses
 {
     
     public partial class SaveCourse
     {
-        [Parameter]
-        public int? Id { get; set; }
-
-        private CourseDto _course = new();
-
-        [Inject] 
-        private  HttpClient _client { get; set; }
+        public SaveCourse():base("Course")
+        {
+            
+        }
         
         [Inject] 
-        private  NavigationManager _Manager { get; set; }
-
-        protected override async Task OnInitializedAsync()
-        {
-            if (Id.HasValue)
-            {
-                _course = (await _client.GetFromJsonAsync<CourseDto>($"Course/{Id.Value}"))!;
-            }
-        }
+        private  NavigationManager _Manager { get; set; }       
 
         private async Task SubmitValidForm()
         {
-            if (Id.HasValue)
-            {
-                await _client.PutAsJsonAsync($"/Course/{Id.Value}", _course);
-            }
-            else
-            {
-               await _client.PostAsJsonAsync("/Course", _course);
-            }
-            _Manager.NavigateTo("/Course/List");
+             var sucess=await  Save();
+             if(sucess){
+                _Manager.NavigateTo("/Course/List");
+             }
+             else{
+
+             }
+            
         }
             
     }
